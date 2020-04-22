601357a0bce421f34ebba9e5d1def82d7174e1f4
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2020-04-22 23:34:44 +0300

    Updated favicon

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,14 @@
 import React from 'react'
 
 import HomeServices from './HomeServices'
+import homepage from './data/homepage'
 
 const Main = () => {
     return (
         <main className='main'>
             <section className='section'>
-                <h1 className='home-h1'>Hi, I'm Corneliu, and I'm a Developer</h1>
-                <p className='intro'>
-                    Nunc a molestie nunc. Sed imperdiet, justo at sagittis cursus, sapien nibh sollicitudin sem, eget consequat turpis orci id metus. Quisque sit amet iaculis libero, in accumsan nunc. Aliquam ipsum mi, convallis a euismod at, molestie quis eros. Suspendisse ullamcorper bibendum sodales. Suspendisse vitae metus mauris. Aliquam erat volutpat. Fusce orci ex, euismod vel magna nec, placerat ultrices tortor. Cras tempus eros non eros interdum, vitae aliquam enim placerat. Nulla tristique elementum lacus, id scelerisque nibh consectetur ut.
-                </p>
+                <h1 className='home-h1'>{homepage.title}</h1>
+                <p className='intro'>{homepage.text}</p>
             </section>
 
             <HomeServices />
--- src/components/singlePage.js
+++ src/components/singlePage.js
@@ -8,6 +8,7 @@ import IconSocial from './icons/IconSocial'
 import IconLogo from './icons/IconLogo'
 import BackdropSVG from './backdropSVG'
 import Button from './Button'
+import homepage from './data/homepage'
 
 const SinglePage = () => {
 
@@ -85,10 +86,8 @@ const SinglePage = () => {
                 </header>
 
                 <main className='main'>
-                    <h1 className='home-h1'>Hi, I'm Corneliu, and I'm a Developer</h1>
-                    <p className='intro'>
-                        Nunc a molestie nunc. Sed imperdiet, justo at sagittis cursus, sapien nibh sollicitudin sem, eget consequat turpis orci id metus. Quisque sit amet iaculis libero, in accumsan nunc. Aliquam ipsum mi, convallis a euismod at, molestie quis eros. Suspendisse ullamcorper bibendum sodales. Suspendisse vitae metus mauris. Aliquam erat volutpat. Fusce orci ex, euismod vel magna nec, placerat ultrices tortor. Cras tempus eros non eros interdum, vitae aliquam enim placerat.
-                    </p>
+                    <h1 className='home-h1'>{homepage.title}</h1>
+                    <p className='intro'>{homepage.text}</p>
                 </main>
 
                 <footer className='footer' style={{ height: footerHeight }}>
