86b3fe3a393aed904a718a30878bcae3bfbb6550
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2020-04-26 10:43:22 +0300

    Added gh-pages support

--- src/components/data/homepage.js
+++ src/components/data/homepage.js
@@ -1,6 +1,4 @@
 export default {
     title: `Hi, I'm Corneliu, and I'm a Full-Stack Designer`,
-    text: `
-        Pellentesque sodales odio eget augue cursus, et maximus quam pharetra. Vivamus eleifend risus quam, quis pretium nisi euismod ac. Aenean pretium et ipsum vitae elementum. Curabitur eu eros maximus, pretium libero et, hendrerit nisl. Morbi auctor sapien ut orci tristique hendrerit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tincidunt condimentum sem, vel tempor metus gravida ut.
-    `
+    text: ``
 }
