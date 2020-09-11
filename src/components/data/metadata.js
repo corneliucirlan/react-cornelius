d7f049086810881d557c3dedcd96b04f7e5eba65
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2020-09-11 12:47:31 +0300

    Updated metadata

--- src/components/data/metadata.js
+++ src/components/data/metadata.js
@@ -1,30 +1,33 @@
+const 	fullName = 'Corneliu Cîrlan',
+		jobTitle = 'Designer. Developer'
+
 export default {
-    title: 'Corneliu Cîrlan - Designer. Developer.',
-    description: 'Metadata description',
-    image: '',
-    url: 'https://www.corneliucirlan.com',
-    schema: {
-        '@context': 'https://schema.org/',
-        '@type': 'Person',
-        'name': 'Corneliu Cîrlan',
-        'url': 'https://www.corneliucirlan.com',
-        'image': '',
-        'sameAs': [
-            'https://www.facebook.com/corneliucirlan/',
-            'https://twitter.com/corneliucirlan',
-            'https://www.instagram.com/corneliucirlan/',
-            'https://www.linkedin.com/in/corneliucirlan/',
-            'https://github.com/corneliucirlan',
-            'https://dribbble.com/corneliucirlan',
-            'https://www.behance.net/corneliucirlan'
-        ],
-        'jobTitle': 'Designer. Developer',
-        'worksFor': {
-            '@type': 'Organization',
-            'name': 'Cîrlan Corneliu PFA'
-        }
+  title: fullName + " - " + jobTitle,
+  description: "Corneliu is a designer working with individuals and businesses to grow their worth.",
+  image: "",
+  url: "https://www.corneliucirlan.com",
+  schema: {
+    "@context": "https://schema.org/",
+    "@type": "Person",
+    name: fullName,
+    url: "https://www.corneliucirlan.com",
+    image: "",
+    sameAs: [
+      "https://www.facebook.com/corneliucirlan",
+      "https://twitter.com/corneliucirlan",
+      "https://www.instagram.com/corneliucirlan",
+      "https://www.linkedin.com/in/corneliucirlan",
+      "https://github.com/corneliucirlan",
+      "https://dribbble.com/corneliucirlan",
+      "https://www.behance.net/corneliucirlan",
+    ],
+    jobTitle: jobTitle,
+    worksFor: {
+      "@type": "Organization",
+      name: "Cîrlan Corneliu PFA",
     },
-    imageAlt: 'Corneliu Cîrlan',
-    facebookAppID: '521959071625533',
-    twitterUsername: '@corneliucirlan'
-}
+  },
+  imageAlt: fullName,
+  facebookAppID: "521959071625533",
+  twitterUsername: "@corneliucirlan",
+};
