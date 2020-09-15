cea1d5b564c0a6cbb24016984b153940fce598ca
Corneliu Cîrlan <corneliu@corneliucirlan.com>
2020-09-15 17:34:43 +0300

    Added copy

--- src/components/data/homepage.js
+++ src/components/data/homepage.js
@@ -1,4 +1,4 @@
 export default {
-    title: `Hi, I'm Corneliu, and I'm a Full-Stack Designer`,
-    text: ``
-}
+  title: `Hi, I'm Corneliu, and I'm a Full-Stack Designer`,
+  text: `As a designer, I help individuals and businesses with everything they need to grow their business, from branding, design, development, search engine optimisation, social media, and maintenance.`,
+};
